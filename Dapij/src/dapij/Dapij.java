20fa9cc73ced06aed5c2a65fff71e480515f84fe
Nikolay <nikolay@Albert.(none)>
2012-06-19 15:55:16 +0100

    Simple example code from Martin

--- Dapij/src/dapij/Dapij.java
+++ Dapij/src/dapij/Dapij.java
@@ -4,6 +4,10 @@
  */
 package dapij;
 
+import java.io.IOException;
+import java.io.InputStream;
+import org.objectweb.asm.*;
+
 /**
  *
  * @author Nikolay Pulev
@@ -13,8 +17,38 @@ public class Dapij {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // simple commit
-        // TODO code application logic here
+    public static int a = 5;
+    
+    public static void main(String[] args) throws IOException {
+       InputStream is = Dapij.class.getResourceAsStream("NewClass.class");
+       ClassReader cr = new ClassReader(is);
+       ClassWriter cw = new ClassWriter(0); //TODO: Read javadoc
+       
+       
+       ClassVisitor cv = new ClassVisitor(Opcodes.ASM4, cw) {
+
+            @Override
+            public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+                System.err.println(superName);
+                super.visit(version, access, name, signature, superName, interfaces);
+            }
+           
+        public FieldVisitor visitField(int access, String name,
+                        String desc,
+                        String signature,
+                        Object value) {
+            System.out.println("Name: " + name);
+            return super.visitField(access, name, desc, signature, value);
+        }
+
+                @Override
+                public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+                    System.out.println("Method " + name);
+                    return super.visitMethod(access, name, desc, signature, exceptions);
+                }
+
+        };
+       cr.accept(cv, 0);
+       System.out.println("Class Length: " + cw.toByteArray().length);
     }
 }
