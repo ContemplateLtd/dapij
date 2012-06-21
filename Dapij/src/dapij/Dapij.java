4645fad35f1f8f52994ce636567f27c8f1c517e6
Nikolay Pulev <>
2012-06-21 13:08:51 +0100

    Attempt  to build Agent + output changes to external visitor

--- Dapij/src/dapij/Dapij.java
+++ Dapij/src/dapij/Dapij.java
@@ -3,60 +3,42 @@
  * and open the template in the editor.
  */
 package dapij;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import dapij.StatsCollector;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
 import org.objectweb.asm.*;
 
 /**
  *
  * @author Nikolay Pulev
  */
-public class Dapij {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static int a = 5;
-
-    public static void main(String[] args) throws IOException {
-        InputStream is = Dapij.class.getResourceAsStream("HelloAzura.class");
-        ClassReader cr = new ClassReader(is);
-        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES); //TODO: Read javadoc
-
-
-        ClassVisitor cv = new ClassVisitor(Opcodes.ASM4, cw) {
-
-            @Override
-            public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-                System.err.println(superName);
-                super.visit(version, access, name, signature, superName, interfaces);
-            }
-
-            @Override
-            public FieldVisitor visitField(int access, String name,
-                    String desc,
-                    String signature,
-                    Object value) {
-                System.out.println("Name: " + name);
-                return super.visitField(access, name, desc, signature, value);
-            }
-
-            @Override
-            public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-                System.out.println("Method " + name);
-                MethodVisitor mv;
-                mv = super.visitMethod(access, name, desc, signature, exceptions);
-                if(mv != null)
-                    mv = new ReturnVisitor(mv, name);
-                return mv;
-            }
-        };
-        cr.accept(cv, 0);
-        System.out.println("Class Length: " + cw.toByteArray().length);
-        FileOutputStream fos = new FileOutputStream("HelloAzura.class");
-        fos.write(cw.toByteArray());
-        fos.close();
-    }
+public class Dapij implements ClassFileTransformer {
+	
+	@Override
+	public byte[] transform(ClassLoader loader, String className,
+			Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
+			byte[] classfileBuffer) throws IllegalClassFormatException {
+		System.out.println("Processing class " + className);
+		
+		try {
+			System.out.println("Obtaining Stats from " + classfileBuffer);
+			// scan class binary format to find fields for toString() method
+			ClassReader creader = new ClassReader(classfileBuffer);
+			ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+			ClassVisitor sc_visitor = new StatsCollector(writer);
+			creader.accept(sc_visitor, ClassReader.SKIP_DEBUG);
+			
+			return writer.toByteArray();
+			
+		} catch (IllegalStateException e) {
+			throw new IllegalClassFormatException("Error: " + e.getMessage() +
+				" on class " + classfileBuffer);
+		}  
+	}
+	
+	public static void premain(String arglist, Instrumentation inst) {
+		inst.addTransformer(new Dapij());
+	} 
 }
