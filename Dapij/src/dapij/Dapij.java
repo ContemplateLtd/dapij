ce2c06545044893f26905ce3e3af4d80f96a3890
mpszmczak <mpszmczak@gmail.com>
2012-06-21 08:53:15 +0100

    New example

--- Dapij/src/dapij/Dapij.java
+++ Dapij/src/dapij/Dapij.java
@@ -4,6 +4,7 @@
  */
 package dapij;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import org.objectweb.asm.*;
@@ -18,37 +19,44 @@ public class Dapij {
      * @param args the command line arguments
      */
     public static int a = 5;
-    
+
     public static void main(String[] args) throws IOException {
-       InputStream is = Dapij.class.getResourceAsStream("NewClass.class");
-       ClassReader cr = new ClassReader(is);
-       ClassWriter cw = new ClassWriter(0); //TODO: Read javadoc
-       
-       
-       ClassVisitor cv = new ClassVisitor(Opcodes.ASM4, cw) {
+        InputStream is = Dapij.class.getResourceAsStream("HelloAzura.class");
+        ClassReader cr = new ClassReader(is);
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES); //TODO: Read javadoc
+
+
+        ClassVisitor cv = new ClassVisitor(Opcodes.ASM4, cw) {
 
             @Override
             public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
                 System.err.println(superName);
                 super.visit(version, access, name, signature, superName, interfaces);
             }
-           
-        public FieldVisitor visitField(int access, String name,
-                        String desc,
-                        String signature,
-                        Object value) {
-            System.out.println("Name: " + name);
-            return super.visitField(access, name, desc, signature, value);
-        }
 
-                @Override
-                public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-                    System.out.println("Method " + name);
-                    return super.visitMethod(access, name, desc, signature, exceptions);
-                }
+            @Override
+            public FieldVisitor visitField(int access, String name,
+                    String desc,
+                    String signature,
+                    Object value) {
+                System.out.println("Name: " + name);
+                return super.visitField(access, name, desc, signature, value);
+            }
 
+            @Override
+            public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+                System.out.println("Method " + name);
+                MethodVisitor mv;
+                mv = super.visitMethod(access, name, desc, signature, exceptions);
+                if(mv != null)
+                    mv = new ReturnVisitor(mv, name);
+                return mv;
+            }
         };
-       cr.accept(cv, 0);
-       System.out.println("Class Length: " + cw.toByteArray().length);
+        cr.accept(cv, 0);
+        System.out.println("Class Length: " + cw.toByteArray().length);
+        FileOutputStream fos = new FileOutputStream("HelloAzura.class");
+        fos.write(cw.toByteArray());
+        fos.close();
     }
 }
---
+++ Dapij/src/dapij/ReturnVisitor.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dapij;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+/**
+ *
+ * @author emszy
+ */
+public class ReturnVisitor extends MethodVisitor {
+    
+    private String name;
+    
+    public ReturnVisitor(MethodVisitor mv, String name)
+    {
+        super(Opcodes.ASM4, mv);
+        this.name = name;
+    }
+    
+    @Override
+    public void visitInsn(int opcode)
+    {
+        if((opcode == Opcodes.RETURN)||(opcode == Opcodes.IRETURN)||(opcode == Opcodes.ARETURN)||(opcode == Opcodes.DRETURN)||(opcode == Opcodes.FRETURN))
+        {
+            mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitLdcInsn("Exiting method " + name);
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V");
+        }
+        mv.visitInsn(opcode);
+    }
+    
+}
