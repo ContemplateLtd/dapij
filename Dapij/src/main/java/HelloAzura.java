5c61f9a32c51026ce6712524f6828d60b3eb21ab
Nikolay Pulev <>
2012-06-22 15:28:52 +0100

    Code formatted according to standards

--- Dapij/src/main/java/HelloAzura.java
+++ Dapij/src/main/java/HelloAzura.java
@@ -1,22 +1,25 @@
-public class HelloAzura {
-  
-  //This has to be compiled and put in the build/classes/dapij directory
-  
-  public static int square(int input)
-  {
-    return input*input;
-  }
-  
-  public static void azura()
-  {
-	  //Azura is the name of my dog
-	  System.out.println("Hello Azura");
-  }
-	
-  public static void main(String args[]) {
-	  
-	System.out.println("The square of 5 is " + square(5));
-    azura();
-  }
-}
+/*
+ * TODO: enter sensible info
+ */
+// package xxx;
 
+/**
+ *
+ * @author emszy
+ */
+public class HelloAzura {
+
+    public static int square(int input) {
+        return input * input;
+    }
+
+    public static void azura() {  
+        /* Azura is the name of my dog */
+        System.out.println("Hello Azura");
+    }
+
+    public static void main(String args[]) {
+        System.out.println("The square of 5 is " + square(5));
+        azura();
+    }
+}
--- Dapij/src/main/java/dapij/Dapij.java
+++ Dapij/src/main/java/dapij/Dapij.java
@@ -1,9 +1,8 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * TODO: enter sensible info 
  */
 package dapij;
-import dapij.StatsCollector;
+
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
@@ -15,30 +14,29 @@ import org.objectweb.asm.*;
  * @author Nikolay Pulev
  */
 public class Dapij implements ClassFileTransformer {
-	
-	@Override
-	public byte[] transform(ClassLoader loader, String className,
-			Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
-			byte[] classfileBuffer) throws IllegalClassFormatException {
-		System.out.println("Processing class " + className);
-		
-		try {
-			System.out.println("Obtaining Stats from " + classfileBuffer);
-			// scan class binary format to find fields for toString() method
-			ClassReader creader = new ClassReader(classfileBuffer);
-			ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-			ClassVisitor sc_visitor = new StatsCollector(writer);
-			creader.accept(sc_visitor, ClassReader.SKIP_DEBUG);
-			
-			return writer.toByteArray();
-			
-		} catch (IllegalStateException e) {
-			throw new IllegalClassFormatException("Error: " + e.getMessage() +
-				" on class " + classfileBuffer);
-		}  
-	}
-	
-	public static void premain(String arglist, Instrumentation inst) {
-		inst.addTransformer(new Dapij());
-	}
+
+    @Override
+    public byte[] transform(ClassLoader loader, String className,
+            Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
+            byte[] classfileBuffer) throws IllegalClassFormatException {
+        System.out.println("Processing class " + className);
+
+        try {
+            System.out.println("Obtaining Stats from " + classfileBuffer);
+            ClassReader creader = new ClassReader(classfileBuffer);
+            ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+            ClassVisitor sc_visitor = new StatsCollector(writer);
+            creader.accept(sc_visitor, ClassReader.SKIP_DEBUG);
+
+            return writer.toByteArray();
+            
+        } catch (IllegalStateException e) {
+            throw new IllegalClassFormatException("Error: " + e.getMessage()
+                    + " on class " + classfileBuffer);
+        }
+    }
+
+    public static void premain(String arglist, Instrumentation inst) {
+        inst.addTransformer(new Dapij());
+    }
 }
